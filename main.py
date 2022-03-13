26b14611d49f604a246e6689ecf278fff08730fa
ricky <chenricky@gmail.com>
2022-03-13 16:43:30 -0700

    add turtle list

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from turtle import Turtle
 from turtle import Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Enter a color of your choice")
 colours = ["red", "green", "blue", "black", "brown"]
 start_position = [50, 30, 10, -10, -30]
+distance = [5, 6, 10, 15, 12, 20]
+race_turtle_list = []
 
 # tim = Turtle(shape="turtle")
 # tim.penup()
@@ -16,5 +19,11 @@ for i in range(0, 5):
     turtle.color(colours[i])
     turtle.penup()
     turtle.goto(x=-230, y=start_position[i])
+    race_turtle_list.append(turtle)
+    print(len(race_turtle_list))
+
+
+
+
 
 screen.exitonclick()

017df81a55b6d82f3c82d85b63d8911d456abb8a
ricky <chenricky@gmail.com>
2022-03-16 21:02:34 -0700

    added some racing logic

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ start_position = [50, 30, 10, -10, -30]
 distance = [50, 60, 10, 15, 12, 20]
 race_turtle_list = []
 
+
 # tim = Turtle(shape="turtle")
 # tim.penup()
 # tim.goto(x=-230, y=-100)
@@ -21,11 +22,15 @@ for i in range(0,5):
     turtle.goto(x=-230, y=start_position[i])
     race_turtle_list.append(turtle)
     print(len(race_turtle_list))
+    print(turtle.position()[0])
 
-for j in range(0,5):
+for j in range(1,6):
     for i in race_turtle_list:
-        i.forward(random.choice(distance))
-        print("move  #" + str(j))
+        move_distance = random.choice(distance)
+        i.forward(move_distance)
+        print(str(i) + " move  #" + str(j) + " distance is: " + str(move_distance))
+        print(i.position()[0])
+
 
 
 

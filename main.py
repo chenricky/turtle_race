56019362805416363dd88180aa201fae3d1b5e9f
ricky <chenricky@gmail.com>
2022-03-16 20:40:45 -0700

    add a for loop to move each turtle at a random distance

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Enter a color of your choice")
 colours = ["red", "green", "blue", "black", "brown"]
 start_position = [50, 30, 10, -10, -30]
-distance = [5, 6, 10, 15, 12, 20]
+distance = [50, 60, 10, 15, 12, 20]
 race_turtle_list = []
 
 # tim = Turtle(shape="turtle")
 # tim.penup()
 # tim.goto(x=-230, y=-100)
 
-for i in range(0, 5):
+for i in range(0,5):
     turtle = Turtle(shape="turtle")
     turtle.color(colours[i])
     turtle.penup()
     turtle.goto(x=-230, y=start_position[i])
     race_turtle_list.append(turtle)
     print(len(race_turtle_list))
+
+for j in range(0,5):
+    for i in race_turtle_list:
+        i.forward(random.choice(distance))
+        print("move  #" + str(j))
+
 
 
 

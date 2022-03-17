e410ca0c138d39fa20ffbe5bdaa6c088aa6fd36f
ricky <chenricky@gmail.com>
2022-03-16 21:33:27 -0700

    added final racing and result logic

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Enter a color of your choice")
+user_bet = screen.textinput(title="Make your bet", prompt="from 1 to 5, enter which turtle win")
 colours = ["red", "green", "blue", "black", "brown"]
 start_position = [50, 30, 10, -10, -30]
 distance = [50, 60, 10, 15, 12, 20]
@@ -28,13 +28,20 @@ for j in range(1,6):
     for i in race_turtle_list:
         move_distance = random.choice(distance)
         i.forward(move_distance)
-        print(str(i) + " move  #" + str(j) + " distance is: " + str(move_distance))
-        print(i.position()[0])
+        print(" move  #" + str(j) + " distance is: " + str(move_distance))
+        print("current turtle X position is: " + str(i.position()[0]))
         if j ==5:
             name.append(i.position()[0])
 
 winning_position = int(name.index(max(name)))+1
+
+print("final position list")
+print(name)
 print("the " + str(winning_position) + " position turtle won!")
+if winning_position == int(user_bet):
+    print("you won the bet")
+else:
+    print("you didn't win the bet")
 
 
 
